970fbfa6949e9c37bdc31388956cbebf804cdadf
nickcheng <n@nickcheng.com>
2014-06-18 15:57:51 +0800

    Header file.

---
+++ NCWeibo/NCWeibo.h
@@ -0,0 +1,19 @@
+//
+//  NCWeibo.h
+//  NextDay
+//
+//  Created by nickcheng on 14-6-18.
+//  Copyright (c) 2014年 nxmix.com. All rights reserved.
+//
+
+#import "NCWeiboClient.h"
+#import "NCWeiboErrorResponse.h"
+#import "NCWeiboAuthentication.h"
+#import "NCWeiboUser.h"
+#import "NCWeiboClient/NCWeiboClient.h"
+#import "NCWeiboClient/NCWeiboClient+HandlerBlocks.h"
+#import "NCWeiboClient/NCWeiboClientConfig.h"
+#import "NCWeiboClient/NCWeiboWebAuthViewController.h"
+#import "NCWeiboClient/API/NCWeiboClient+Friendship.h"
+#import "NCWeiboClient/API/NCWeiboClient+Status.h"
+#import "NCWeiboClient/API/NCWeiboClient+User.h"
