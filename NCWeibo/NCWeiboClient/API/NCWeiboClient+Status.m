265351336dc2f670896dc97f7ba398e7597e7a39
nickcheng <n@nickcheng.com>
2016-11-10 22:52:09 +0900

    [WIP] First API call.

--- NCWeibo/NCWeiboClient/API/NCWeiboClient+Status.m
+++ NCWeibo/NCWeiboClient/API/NCWeiboClient+Status.m
@@ -8,6 +8,8 @@
 
 #import "NCWeiboClient+Status.h"
 #import "NCWeiboClientConfig.h"
+#import "WeiboSDK.h"
+
 
 @implementation NCWeiboClient (Status)
 
@@ -20,99 +22,37 @@
 }
 
 - (void)createTextStatusWithText:(NSString *)text completion:(NCWeiboClientCompletionBlock)completionHandler {
-  [self doAuthBeforeCallAPI:^{
-    NSDictionary *params = @{
-                             @"status": text,
-                             };
-    [self POST:@"statuses/update.json"
-        parameters:params
-           success:[self successHandlerForClientHandler:completionHandler]
-           failure:[self failureHandlerForClientHandler:completionHandler]];
-  } andAuthErrorProcess:completionHandler];
+    [self doAuthBeforeCallAPI:^{
+        [WBHttpRequest
+         requestForShareAStatus:text
+         contatinsAPicture:nil
+         orPictureUrl:nil
+         withAccessToken:self.accessToken
+         andOtherProperties:nil
+         queue:nil
+         withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+             [self processRequestCompletion:httpRequest result:result error:error handler:completionHandler];
+         }];
+    } andAuthErrorProcess:completionHandler];
 }
 
 - (void)createImageStatusWithImage:(UIImage *)image andText:(NSString *)text completion:(NCWeiboClientCompletionBlock)completionHandler {
-  [self doAuthBeforeCallAPI:^{
-    NSDictionary *params = @{
-                             @"status": text,
-                             };
-    /// This is standard way.
-//    [self POST:@"statuses/upload.json"
-//        parameters:params
-//        constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-//          NSData *data = UIImageJPEGRepresentation(image, 1.0);
-//          [formData appendPartWithFileData:data
-//                                      name:@"pic"
-//                                  fileName:NCWEIBO_UPLOADIMAGENAME
-//                                  mimeType:@"image/jpeg"];
-//        }
-//        success:[self successHandlerForClientHandler:completionHandler]
-//        failure:[self failureHandlerForClientHandler:completionHandler]];
-    
-    /// Walkaround for Apple bug. See below link for details.
-    /// @see https://github.com/AFNetworking/AFNetworking/issues/1398
-    NSString* apiUrl = [[NSURL URLWithString:@"statuses/upload.json" relativeToURL:self.baseURL] absoluteString];
-    
-    // Prepare a temporary file to store the multipart request prior to sending it to the server due to an alleged
-    // bug in NSURLSessionTask.
-    NSString* tmpFilename = [NSString stringWithFormat:@"%f", [NSDate timeIntervalSinceReferenceDate]];
-    NSURL* tmpFileUrl = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:tmpFilename]];
-    
-    // Create a multipart form request.
-    NSMutableURLRequest *multipartRequest =
-      [self.requestSerializer multipartFormRequestWithMethod:@"POST"
-                                                   URLString:apiUrl
-                                                  parameters:params
-                                   constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-                                     NSData *data = UIImageJPEGRepresentation(image, 1.0);
-                                     [formData appendPartWithFileData:data
-                                                                 name:@"pic"
-                                                             fileName:NCWEIBO_UPLOADIMAGENAME
-                                                             mimeType:@"image/jpeg"];
-                                   } error:nil];
-    
-    // Dump multipart request into the temporary file.
-    [self.requestSerializer
-        requestWithMultipartFormRequest:multipartRequest
-        writingStreamContentsToFile:tmpFileUrl
-        completionHandler:^(NSError *error) {
-          // Once the multipart form is serialized into a temporary file, we can initialize
-          // the actual HTTP request using session manager.
-          
-          // Create default session manager.
-          AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-          
-          // Show progress.
-          NSProgress *progress = nil;
-          
-          // Here note that we are submitting the initial multipart request. We are, however,
-          // forcing the body stream to be read from the temporary file.
-          NSURLSessionUploadTask *uploadTask =
-            [manager uploadTaskWithRequest:multipartRequest
-                                  fromFile:tmpFileUrl
-                                  progress:&progress
-                         completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
-                           // Cleanup: remove temporary file.
-                           [[NSFileManager defaultManager] removeItemAtURL:tmpFileUrl error:nil];
-                           
-                           // Do something with the result.
-                           if (error) {
-                             NCLogInfo(@"Error: %@", error);
-                           } else {
-                             NCLogInfo(@"Success: %@", responseObject);
-                           }
-                         }];
-          
-          // Add the observer monitoring the upload progress.
-          [progress addObserver:self
-                     forKeyPath:@"fractionCompleted"
-                        options:NSKeyValueObservingOptionNew
-                        context:NULL];
-          
-          // Start the file upload.
-          [uploadTask resume];
-        }];
-  } andAuthErrorProcess:completionHandler];
+    [self doAuthBeforeCallAPI:^{
+        WBImageObject *imageObject = [WBImageObject object];
+        imageObject.imageData = UIImagePNGRepresentation(image);
+        
+        //
+        [WBHttpRequest
+         requestForShareAStatus:text
+         contatinsAPicture:imageObject
+         orPictureUrl:nil
+         withAccessToken:self.accessToken
+         andOtherProperties:nil
+         queue:nil
+         withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+             [self processRequestCompletion:httpRequest result:result error:error handler:completionHandler];
+         }];
+    } andAuthErrorProcess:completionHandler];
 }
 
 @end
--- NCWeibo/NCWeiboClient/NCWeiboClient+HandlerBlocks.m
+++ NCWeibo/NCWeiboClient/NCWeiboClient+HandlerBlocks.m
@@ -10,41 +10,21 @@
 #import "NCWeiboErrorResponse.h"
 #import "NCWeiboClientConfig.h"
 
+
 @implementation NCWeiboClient (HandlerBlocks)
 
-- (AFNetworkingSuccessBlock)successHandlerForClientHandler:(NCWeiboClientCompletionBlock)handler {
-  return ^(NSURLSessionDataTask *task, id responseObject) {
-    [self processSuccessHandlerWithRequestOperation:task andResponseObject:responseObject andHandler:handler];
-  };
-}
-
-- (void)processSuccessHandlerWithRequestOperation:(NSURLSessionDataTask *)task andResponseObject:(id)responseObject andHandler:(NCWeiboClientCompletionBlock)handler {
-  //
-  NCLogInfo(@"%@", task.currentRequest.allHTTPHeaderFields);
-  
-  //
-  if (handler)
-    handler(task, responseObject, nil);
-}
-
-- (AFNetworkingFailureBlock)failureHandlerForClientHandler:(NCWeiboClientCompletionBlock)handler {
-  return ^(NSURLSessionDataTask *task,  NSError *error) {
-//    NCLogInfo(@"%@", operation.request.allHTTPHeaderFields);
-    NCWeiboErrorResponse *errorResponse = [[NCWeiboErrorResponse alloc] initWithJson:error.userInfo[NSLocalizedRecoverySuggestionErrorKey]];
-
-    // Check error code. If code means token expired, call expired block.
-//    errorResponse.errorCode = 21327; // Test code. Remove this when release.
-    if (errorResponse && errorResponse.errorCode == 21327) {
-      NCLogError(@"Auth Token expired. Will call accessTokenExpiredHandler.");
-      if (self.accessTokenExpiredHandler)
-        self.accessTokenExpiredHandler(self.originalAPICallBlock);
+- (void)processRequestCompletion:(WBHttpRequest *)httpRequest result:(id)result error:(NSError *)error handler:(NCWeiboClientCompletionBlock)handler {
+    if (error) {
+        NCLogError(@"Request:%@ \n Error:%@", httpRequest, error);
+        if (handler) {
+            handler(result, error);
+        }
+    } else {
+        NCLogInfo(@"Request:%@", httpRequest);
+        if (handler) {
+            handler(result, nil);
+        }
     }
-
-    //
-    if (handler) {
-      handler(task, errorResponse, error);
-    }
-  };
 }
 
 - (void)doAuthBeforeCallAPI:(APIHandlerBlock)apiHandler andAuthErrorProcess:(NCWeiboClientCompletionBlock)completionHandler {
